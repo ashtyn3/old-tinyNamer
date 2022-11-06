48c3212940e77fa7045cfde90822358a9aa84284
ashtyn3 <ashtynmorel@gmail.com>
2022-11-06 16:28:45 -0500

    refactor: console works with new socket class

--- src/console.py
+++ src/console.py
@@ -2,21 +2,21 @@ from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
-import sock
-import base58
-from threading import Thread
-from pprint import pprint
+import con_sock as sock
 from appdirs import *
 import os
 import os.path as path
 from prompt_toolkit.key_binding import KeyBindings
+from pathlib import Path
+import sys
 
-appname = "blogd"
-appauthor = "blogd"
+appname = "tinyNamer"
+appauthor = "tinyNamer"
 
 bindings = KeyBindings()
 
-cache = user_cache_dir(appname, appauthor)
+cache = str(Path.home().absolute()) + "/.tinyNamer"
+
 if not path.exists(cache):
     os.mkdir(cache)
 
@@ -29,6 +29,8 @@ session = PromptSession(
 ) 
 prefered_client = ("", 8001)
 n = sock.node(True)
+global PEER
+PEER = None
 
 
 words = WordCompleter(
@@ -41,30 +43,31 @@ words = WordCompleter(
     ignore_case=True,
 )
 
-def msg(body: str):
-    t = Thread(target=n.run)
-    t.start()
-    pprint(base58.b58decode(str(n.msg_peer(body, prefered_client, True))).decode("utf-8"), indent=4)
-
-
-while True:
-    # Do multiple input calls.
-    prompt: str = session.prompt("> ", completer=words, complete_while_typing=True, key_bindings=bindings, mouse_support=True)
-    frags = prompt.strip().split(" ")
-    match frags[0]:
-        case "exit":
-            break
-        case "connect":
-            if len(frags) == 1:
-                print("connect expects an argument.")
+try:
+    while True:
+        # Do multiple input calls.
+        prompt: str = session.prompt("> ", completer=words, complete_while_typing=True, key_bindings=bindings, mouse_support=True)
+        frags = prompt.strip().split(" ")
+        match frags[0]:
+            case "exit":
                 break
-            parts = frags[1].split(":")
-            prefered_client = (parts[0], int(parts[1]))
-        case "full":
-            print("blocking...")
-            n.is_light = False
-            n.gen_id(n.server)
-            n.run()
-        case "get_peers":
-            msg("get_peers")
-        
+            case "connect":
+                if len(frags) == 1:
+                    print("connect expects an argument.")
+                    break
+                parts = frags[1].split(":")
+                prefered_client = (parts[0], int(parts[1]))
+                PEER = n.peers.add_peer(sock.peer.Peer(n.id, from_tuple=True, tup=prefered_client))
+                if PEER:
+                    print(PEER.read())
+            case "full":
+                print("blocking...")
+                n.is_light = False
+                n.gen_id(n.server)
+                n.run()
+            case "get_peers":
+                if PEER:
+                    PEER.msg(b"get_peers")
+                    print(PEER.read())
+except KeyboardInterrupt:
+    sys.exit()
